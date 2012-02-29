acdb6e946fbefd4a296effc59a1bcbb7ed61d5f5
oscarfroberg <oscarfroberg@gmail.com>
2012-02-29 20:50:30 +0200

    First commit

---
+++ arduinopcremote.ino
@@ -0,0 +1,77 @@
+#include <IRremote.h> // Copyright 2009 Ken Shirriff http://www.arcfn.com/2009/08/multi-protocol-infrared-remote-library.html
+
+int RECV_PIN = 11;
+int RELAY_PIN = 8;
+int RELAY_STATUS = 0;
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
+
+void setup()
+{
+  Serial.begin(115200);
+  irrecv.enableIRIn(); // Start the receiver
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, LOW);
+}
+
+void loop() {
+    
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, DEC);
+    if ((results.value) == 16753245) {
+      if (RELAY_STATUS == 0) {
+        digitalWrite(RELAY_PIN, HIGH);
+        RELAY_STATUS = 1;
+        Serial.println("F"); // power off
+      }
+      else if (RELAY_STATUS == 1) {
+        digitalWrite(RELAY_PIN, LOW);
+        RELAY_STATUS = 0;
+        Serial.println("O"); // power on
+      } 
+    }
+  
+  switch (results.value) {
+    case 16761405:
+      Serial.println("M"); // play-pause
+      break;
+    case 16769565:
+      Serial.println("M"); // mute
+      break;
+    case 16754775:
+      Serial.println("u"); // software vol up
+      break;
+    case 16769055:
+      Serial.println("d"); // software vol down
+      break;
+    case 16750695:
+      Serial.println("U"); // master vol up
+      break;
+    case 16738455:
+      Serial.println("D"); // master vol down
+      break;
+    case 16720605:
+      Serial.println("P"); // previous song
+      break;
+    case 16712445:
+      Serial.println("N"); // next song
+      break;
+    case 16724175:
+      Serial.println("s"); // start music player
+      break;
+    case 16756815:
+      Serial.println("q"); // quit media and music player
+      break;
+    case 16736925:
+      Serial.println("f"); // fullscreen
+      break;
+    case 16728765:
+      Serial.println("b"); // vlc back 3 seconds
+  }
+
+  irrecv.resume(); // Receive the next value
+  }
+  
+}
---
+++ ir-remote.py
@@ -0,0 +1,78 @@
+#! /usr/bin/env python
+
+# Read the output of an Arduino which may be printing sensor output,
+# and at the same time, monitor the user's input and send it to the Arduino.
+# See also
+# http://www.arcfn.com/2009/06/arduino-sheevaplug-cool-hardware.html
+
+import sys, serial, select, subprocess, os
+
+class Arduino() :
+  def run(self) :
+    # Port may vary, so look for it:
+    baseport = "/dev/ttyACM"
+    self.ser = None
+    for i in xrange(0, 9) :
+      try :
+        self.ser = serial.Serial(baseport + str(i), 115200, timeout=2)
+        break
+      except :
+        self.ser = None
+        pass
+
+    if not self.ser :
+      print "Couldn't open a serial port"
+      sys.exit(1)
+    print "Opened /dev/ttyUSB" + str(i)
+
+    self.ser.flushInput()
+      while True :
+        inp, outp, err = select.select([sys.stdin, self.ser], [], [], .2)
+
+        # If the user has typed anything, send it to the Arduino:
+        if sys.stdin in inp :
+          line = sys.stdin.readline()
+          self.ser.write(line)
+
+        # If the Arduino has printed anything, display it:
+        if self.ser in inp :
+          line = self.ser.readline().strip()
+          print "Arduino:", line
+  if line == "M":
+    os.system('quodlibet --play-pause')
+    os.system('echo -n  "pause" | nc -U /home/media/vlc.sock')
+  if line == "N":
+    os.system('quodlibet --next')
+    os.system('echo -n "key key-jump+medium" | nc -U /home/media/vlc.sock')
+  if line == "P":
+    os.system('quodlibet --previous')
+    os.system('echo -n "key key-jump-medium" | nc -U /home/media/vlc.sock')
+  if line == "u":
+    os.system('quodlibet --volume-up')
+    os.system('echo -n "volup 2" | nc -U /home/media/vlc.sock')
+  if line == "d":
+    os.system('quodlibet --volume-down')
+    os.system('echo -n "voldown 2" | nc -U /home/media/vlc.sock')
+  if line == "U":
+    os.system('amixer set PCM 10%+ -c 1')
+  if line == "D":
+    os.system('amixer set PCM 10%- -c 1')
+  if line == "q":
+    os.system('quodlibet --quit')
+    os.system('echo -n "quit" | nc -U /home/media/vlc.sock')
+  if line == "f":
+    os.system('echo -n "f" | nc -U /home/media/vlc.sock')
+  if line == "s":
+    os.system('quodlibet &')
+  if line == "b":
+    os.system('echo -n "key key-jump-extrashort" | nc -U /home/media/vlc.sock')
+
+arduino = Arduino()
+try :
+  arduino.run()
+except serial.SerialException :
+  print "Disconnected (Serial exception)"
+except IOError :
+  print "Disconnected (I/O Error)"
+except KeyboardInterrupt :
+  print "Interrupt"
